0112cfbb72c08c17e4b76c769812acb6400c8bdd
Bruno Bizeray <bbizeray@ciandt.com>
2024-04-30 19:15:29 -0300

    feat: add check role to enable or not buttons

---
+++ src/Security/index.ts
@@ -0,0 +1,19 @@
+const hasRole = (role: string) => {
+  const token = localStorage.getItem('token') || '';
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  const roles = JSON.parse(jsonPayload).roles;
+  return roles.includes(role);
+};
+
+export { hasRole };
