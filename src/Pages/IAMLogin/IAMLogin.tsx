fc16c29cce2714b219a4602bc21aeca403270421
Bruno Bizeray <bbizeray@ciandt.com>
2024-05-03 10:13:23 -0300

    style: ajust iam login page

--- src/Pages/IAMLogin/IAMLogin.tsx
+++ src/Pages/IAMLogin/IAMLogin.tsx
@@ -1,6 +1,9 @@
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
 import { useEffect, useState } from "react";
-import "./IAMLogin.scss";
 import { useSearchParams } from "react-router-dom";
+import "./IAMLogin.scss";
 
 export default function IAMLogin() {
   const [searchParams] = useSearchParams();
@@ -14,30 +17,34 @@ export default function IAMLogin() {
     console.log(clientId, clientSecret);
   });
 
-  const login = async () => {};
+  const login = async () => { };
 
   return (
-    <div className="IAMLogin">
-      <div className="IAMLogin__container">
-        <h1 className="IAMLogin__title">Login</h1>
-        <input
-          className="IAMLogin__input"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          className="IAMLogin__input"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="IAMLogin__button" onClick={login}>
-          Login
-        </button>
-        <p className="IAMLogin__error">{error}</p>
+    <div className="iamLogin">
+      <div className="container">
+        <div className="title">
+          <img src="/logo192.png" alt="Tenant Logo"></img>
+          <h3>Sign in to your account</h3>
+        </div>
+        <form action="" className="loginForm">
+          <span>E-mail</span>
+          <InputText
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <span>Password</span>
+          <Password
+            type="password"
+            value={password}
+            feedback={false}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button label="Login"></Button>
+          <div className="forgotPassword">
+            <span>Forgot your password?</span>
+          </div>
+        </form>
       </div>
     </div>
   );
