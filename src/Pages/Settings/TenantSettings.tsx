3b330a2358bc1c6945c3f90aae2680c9f7b0a0d7
Bruno Bizeray <bbizeray@ciandt.com>
2024-05-03 11:36:30 -0300

    feat: add more configs on tenant page

--- src/Pages/Settings/TenantSettings.tsx
+++ src/Pages/Settings/TenantSettings.tsx
@@ -3,9 +3,16 @@ import { FileUpload } from "primereact/fileupload";
 import { InputText } from "primereact/inputtext";
 import Layout from "../../Layout/Layout";
 import "./TenantSettings.scss";
+import { useState } from "react";
+import { ColorPicker } from "primereact/colorpicker";
+import { Button } from "primereact/button";
 
 export default function TenantSettings() {
-  const onUpload = () => {};
+  const [primaryColor, setPrimaryColor] = useState<string>("#000000");
+  const [secondColor, setSecondColor] = useState<string>("#000000");
+  const [textColor, setTextColor] = useState<string>("#000000");
+
+  const onUpload = () => { };
 
   return (
     <Layout>
@@ -18,21 +25,66 @@ export default function TenantSettings() {
               authorization.
             </p>
           </div>
-          <div className="configItem">
-            <span>Name</span>
-            <InputText></InputText>
-          </div>
+          <form>
+            <div className="configItem">
+              <span>Login Image</span>
+              <FileUpload
+                mode="basic"
+                name="demo[]"
+                accept="image/*"
+                maxFileSize={1000000}
+                onUpload={onUpload}
+              />
+            </div>
+            
+            <div className="configItem">
+              <span>Name</span>
+              <InputText></InputText>
+            </div>
 
-          <div className="configItem">
-            <span>Login Image</span>
-            <FileUpload
-              mode="basic"
-              name="demo[]"
-              accept="image/*"
-              maxFileSize={1000000}
-              onUpload={onUpload}
-            />
-          </div>
+            <div className="configItem">
+              <span>Primary Color</span>
+              <div className="color">
+                <ColorPicker format="hex"
+                  value={primaryColor}
+                  onChange={(e: any) => setPrimaryColor(e.value)}
+                  style={{ width: '2em' }} />
+                <InputText value={primaryColor}
+                  onChange={(e: any) => setPrimaryColor(e.target.value)}
+                  style={{ marginLeft: '10px' }}
+                ></InputText>
+              </div>
+            </div>
+
+            <div className="configItem">
+              <span>Second Color</span>
+              <div className="color">
+                <ColorPicker format="hex"
+                  value={secondColor}
+                  onChange={(e: any) => setSecondColor(e.value)}
+                  style={{ width: '2em' }} />
+                <InputText value={secondColor}
+                  onChange={(e: any) => setSecondColor(e.target.value)}
+                  style={{ marginLeft: '10px' }}
+                ></InputText>
+              </div>
+            </div>
+
+            <div className="configItem">
+              <span>Text Color</span>
+              <div className="color">
+                <ColorPicker format="hex"
+                  value={textColor}
+                  onChange={(e: any) => setTextColor(e.value)}
+                  style={{ width: '2em' }} />
+                <InputText value={textColor}
+                  onChange={(e: any) => setTextColor(e.target.value)}
+                  style={{ marginLeft: '10px' }}
+                ></InputText>
+              </div>
+            </div>
+            <Button label="Save" type="submit"></Button>
+          </form>
         </Card>
       </div>
     </Layout>
